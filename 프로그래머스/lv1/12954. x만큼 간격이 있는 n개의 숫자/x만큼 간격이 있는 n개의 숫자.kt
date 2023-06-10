6e92e722237b0ee3dfb7731f5ac49956d88ce915
SangJun Lee <39490416+dltkd1395@users.noreply.github.com>
2023-06-10 17:33:36 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
@@ -1,18 +1,7 @@
 class Solution {
     
     fun solution(x: Int, n: Int): LongArray {
-        val minValue = Long.MIN_VALUE
-        val maxValue = Long.MAX_VALUE
-        val x = x.toLong()
-      
-        return when {
-            x > 0 -> (x..x*n).step(x)
-                .filter { it >= minValue && it <= maxValue }
-                .toLongArray()
-            x < 0 ->  (x downTo x*n).step(-x)
-                .filter { it >= minValue && it <= maxValue }
-                .toLongArray()
-            else -> LongArray(n) { 0 }
-        }
+     
+        return LongArray(n) { x.toLong() * (it+1) }
     }
 }
