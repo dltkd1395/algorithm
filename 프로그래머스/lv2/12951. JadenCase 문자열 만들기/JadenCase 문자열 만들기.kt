362e4074bcfc9d50879de4562dfe1704c46159ca
SangJun Lee <39490416+dltkd1395@users.noreply.github.com>
2023-06-12 23:22:32 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 22.68 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -1,18 +1,9 @@
 class Solution {
     fun solution(s: String): String {
-        var answer = ""
         
-        answer = s[0].uppercase()
-        val s = s.lowercase()
-        
-        for (i in 1 until s.length) {
-            if (s[i-1] == ' ' && s[i].isLetter()) {
-                answer+= s[i].uppercase()
-            } else {
-                answer += s[i]
-            }
-        }
-
-        return answer
+        return s.lowercase()
+                .split(" ")
+                .map { str -> str.replaceFirstChar { it.uppercase() }}
+                .joinToString(" ")
     }
 }
