62f17d65e127cf3cf1bf7804e346098c7cf86581
SangJun Lee <39490416+dltkd1395@users.noreply.github.com>
2023-06-14 03:11:54 +0900

    [level 2] Title: 의상, Time: 4.70 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.kt
+++ 프로그래머스/lv2/42578. 의상/의상.kt
@@ -1,10 +1,8 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-       return clothes.groupingBy { it[1] }
-            .eachCount()
-            .mapValues { it.value + 1}
+       return clothes.groupBy { it[1] }
             .values
-            .toList()
-            .reduce { acc, v -> acc * v } -1
+            .map { it.size + 1}
+            .reduce(Int::times) -1
     }
 }
