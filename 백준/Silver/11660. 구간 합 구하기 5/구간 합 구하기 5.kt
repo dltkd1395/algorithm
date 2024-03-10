d34be2cf2d2c4023a49d2723510c09f841209cf4
SangJun Lee <39490416+hegleB@users.noreply.github.com>
2024-03-10 23:56:56 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2732 ms, Memory: 213152 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -1,43 +1,16 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val dp = List(n + 1) { MutableList(n + 1) { 0 } }
 
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val st = StringTokenizer(br.readLine())
-
-    val n = st.nextToken().toInt()
-    val m = st.nextToken().toInt()
-
-    val matrix = Array(n + 1) { IntArray(n + 1) }
-
-    for (i in 1 until n+1) {
-        val numbers = IntArray(n + 1)
-        val numTokens = StringTokenizer(br.readLine())
-
-        for (j in 1 until n+1) {
-            numbers[j] = numTokens.nextToken().toInt()
-            matrix[i][j] = matrix[i][j-1] + numbers[j]
+    for (i in 1..n) {
+        val line = readLine().split(" ").map { it.toInt() }
+        for (j in 1..n) {
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + line[j - 1]
         }
     }
 
-    for (j in 1 until n+1) {
-        for (i in 2 until n+1) {
-            matrix[i][j] += matrix[i-1][j]
-        }
+    repeat(m) {
+        val (x1, y1, x2, y2) = readLine().split(" ").map { it.toInt() }
+        println(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1])
     }
-    
-    val output = StringBuilder()
-
-    for (i in 0 until m) {
-        val queryTokens = StringTokenizer(br.readLine())
-        val x1 = queryTokens.nextToken().toInt()
-        val y1 = queryTokens.nextToken().toInt()
-        val x2 = queryTokens.nextToken().toInt()
-        val y2 = queryTokens.nextToken().toInt()
-
-        val result = matrix[x2][y2]-matrix[x1-1][y2]-matrix[x2][y1-1]+matrix[x1-1][y1-1]
-        output.append("$result\n")
-    }
-    print(output)
 }
