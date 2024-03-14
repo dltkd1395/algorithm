d097b2d55fe55fbc1ebb4d817e5b848672804a34
SangJun Lee <39490416+hegleB@users.noreply.github.com>
2024-03-15 07:36:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 816 ms, Memory: 127456 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
@@ -1,20 +1,33 @@
+import java.util.StringTokenizer
+
+lateinit var S: String
+var q: Int = 0
+val DP = Array(200002) { IntArray(26) }
+
 fun main() = with(System.`in`.bufferedReader()) {
     val bw = System.out.bufferedWriter()
-    val str = readLine()
-    val q = readLine().toInt()
-    repeat(q) {
-        var (alpha, l, r) = readLine().split(" ")
-        val prefixSum = MutableList(str.length + 1) { 0 }
-        for (i in 1 until str.length + 1) {
-            prefixSum[i] = if (str[i - 1] == alpha.first()) {
-                prefixSum[i - 1] + 1
-            } else {
-                prefixSum[i - 1]
-            }
+    S = readLine()
+    q = readLine().toInt()
+    // 각 알파벳 문자열에 대한 누적합 구하기
+    for (i in 0..25) {
+        for (j in 1..S.length) {
+            DP[j][i] = if (S[j - 1] == 'a' + i) DP[j - 1][i] + 1 else DP[j - 1][i]
         }
-        val left = l.toInt()
-        val right = r.toInt()
-        bw.write("${prefixSum[right + 1] - prefixSum[left]}\n")
     }
+    // 질문 값 저장 및 함수 실행하여 결과 BufferedWriter 저장
+    repeat(q) {
+        val st = StringTokenizer(readLine(), " ")
+        val alphabet = st.nextToken()[0] - 'a'
+        val start = st.nextToken().toInt()
+        val end = st.nextToken().toInt()
+        bw.write("${cal(alphabet, start, end)}\n")
+    }
+
+    bw.flush()
     bw.close()
+    close()
+}
+
+fun cal(alphabet: Int, start: Int, end: Int): Int {
+    return if (start == 0) DP[end + 1][alphabet] else DP[end + 1][alphabet] - DP[start][alphabet]
 }
