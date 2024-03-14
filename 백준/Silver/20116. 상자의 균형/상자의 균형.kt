d67c1171fcb976e0f92bb4a86b0c24ecd98cc83b
SangJun Lee <39490416+hegleB@users.noreply.github.com>
2024-03-14 15:43:14 +0900

    [Silver III] Title: 상자의 균형, Time: 432 ms, Memory: 56404 KB -BaekjoonHub

---
+++ 백준/Silver/20116. 상자의 균형/상자의 균형.kt
@@ -0,0 +1,23 @@
+val br = System.`in`.bufferedReader()
+fun main() = with(System.out.bufferedWriter()){
+
+    val (n,l) = br.readLine().split(' ').map{it.toInt()}
+    val input = br.readLine().split(' ').map{it.toInt()}
+    val pSum = DoubleArray(n)
+    pSum[0] = input[0].toDouble()
+
+    for(i in 1 until n){
+        pSum[i] = pSum[i-1]+input[i]
+    }
+
+    for(i in 0 until n-1){
+        if((pSum[n-1]-pSum[i])/(n-1-i) <= input[i]-l || (pSum[n-1]-pSum[i])/(n-1-i) >= input[i]+l){
+            write("unstable")
+            close()
+            return
+        }
+    }
+    write("stable")
+
+    close()
+}
