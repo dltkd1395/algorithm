b0ac6e22f0f5dd3d965c29c7c06db42d21055baf
SangJun Lee <39490416+hegleB@users.noreply.github.com>
2024-03-16 23:15:42 +0900

    [Gold V] Title: 기차 여행, Time: 836 ms, Memory: 87248 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10713. 기차 여행/기차 여행.kt
+++ 백준/Gold/10713. 기차 여행/기차 여행.kt
@@ -1,24 +1,24 @@
 fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val plan = readLine().split(" ").map { it.toInt() }
     val cost = List(n - 1) { readLine().split(" ").map { it.toInt() } }
     val count = MutableList(n + 1) { 0 }
+
     for (i in 0 until m - 1) {
-        if (plan[i] < plan[i + 1]) {
-            count[plan[i]] += 1
-            count[plan[i + 1]] -= 1
-        } else {
-            count[plan[i]] -= 1
-            count[plan[i + 1]] += 1
-        }
+        val lower = minOf(plan[i], plan[i + 1])
+        val higher = maxOf(plan[i], plan[i + 1])
+        count[lower] += 1
+        count[higher] -= 1
     }
 
     var sum = 0
-    var result = 0
+    var result = 0L
 
     for (i in 0 until n - 1) {
         sum += count[i + 1]
-        result += minOf(cost[i][0] * sum, cost[i][1] * sum + cost[i][2])
+        result += minOf(cost[i][0].toLong() * sum, cost[i][1].toLong() * sum + cost[i][2])
     }
-    println(result)
+
+    bw.use { it.write("$result\n") }
 }
