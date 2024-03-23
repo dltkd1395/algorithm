6d727793452b358a3e9e47a567de7230681480dc
SangJun Lee <39490416+hegleB@users.noreply.github.com>
2024-03-23 19:36:46 +0900

    Time: 238 ms (38.39%), Space: 38.9 MB (20.79%) - LeetHub

--- Leetcode/0001-two-sum/0001-two-sum.kt
+++ Leetcode/0001-two-sum/0001-two-sum.kt
@@ -1,21 +1,29 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         var left = 0
-        var right = 1
+        var right = nums.size - 1
+        data class Position(val idx: Int, val num: Int)
+        val arr = mutableListOf<Position>()
+        for (i in 0 until nums.size) {
+            arr.add(Position(i, nums[i]))
+        }
+        arr.sortWith(compareBy({it.num}))
+        println(arr)
 
-        while(left < right) {
-            println("$left $right")
-            if (nums[left] + nums[right] == target) {
-                return intArrayOf(left, right)
+        while (left <= right) {
+            val (li, num1) = arr[left]
+            val (ri, num2) = arr[right]
+            if (num1 + num2 == target) {
+                return intArrayOf(li, ri)
             }
-            if (left + 1 == right) {
-                right++
-            } else if (right == nums.size - 1) {
+
+            if (num1 + num2 <= target) {
                 left++
             } else {
-                left++
+                right--
             }
         }
+
         return intArrayOf()
     }
 }
